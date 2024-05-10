a7deed0351e90af5ee9b013a50e5d94f5ab7e95b
ThiagooAguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-05-10 12:29:37 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,42 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_0 = len(list_to_remove_elements)
+    if list_0 < 5:
+        return list_to_remove_elements.remove(0)
+    elif 5<list_0 and list_0<6:
+        return list_to_remove_elements.remove(0)
+        return list_to_remove_elements.remove(4)
+    else:
+        return list_to_remove_elements.remove(0)
+        return list_to_remove_elements.remove(4)
+        return list_to_remove_elements.remove(5)
+
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_1 = list_to_add_elements
+    return list_1.append("Yellow") and list_1.insert(0, "Pink")
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_check) > 0:
+        print("no está vacía")
+    else:
+        print("Está vacía")
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+   if list_to_compare1[2]== list_to_compare2[2]:
+        return True
+    else:
+        return False
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+   a=list_of_lists_to_modify
+    del a[0][-1]
+    del a[1][0]
+    del a[1][-1]
+    del a[2][0]
+    del a[2][1]
+    return a
