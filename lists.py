53b1e643e530f578dbee42e720ac236366675d31
ThiagooAguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-05-16 22:46:40 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,42 +1,45 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    list_0 = len(list_to_remove_elements)
-    if list_0 < 5:
-        return list_to_remove_elements.remove(0)
-    elif 5<list_0 and list_0<6:
-        return list_to_remove_elements.remove(0)
-        return list_to_remove_elements.remove(4)
-    else:
-        return list_to_remove_elements.remove(0)
-        return list_to_remove_elements.remove(4)
-        return list_to_remove_elements.remove(5)
+    lista= []
+    
+    # Agregar el primer elemento si existe
+    if len(list_to_remove_elements) > 0:
+        lista.append(lst[0])
+    if len(list_to_remove_elements) > 4:
+        lista.append(lst[4])
+    if len(list_to_remove_elements) > 5:
+        lista.append(lst[5])
+    result = list_to_remove_elements[:]
+    for element in elements_to_remove:
+        result.remove(element)
 
 
 
 def add_elements(list_to_add_elements):
-    list_1 = list_to_add_elements
-    return list_1.append("Yellow") and list_1.insert(0, "Pink")
+    list_to_add_elements.insert(0,"Pink")
+    list_to_add_elements.append("Yellow")
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
     if len(list_to_check) == 0:
         return True 
-    else:
+    elif len(list_to_check) > 0: 
         return False 
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-   if (len(list_to_compare1) + len(list_to_compare2)) > 6 and  list_to_compare1 [2] == list_to_compare2 [2] 
+    if len(list_to_compare1)<3 or len(list_to_compare2)<3:
+        return False
+    elif list_to_compare1[2] == list_to_compare2[2]:
         return True
     else:
-        return False
+        return Flase 
 
 def list_of_lists(list_of_lists_to_modify):
-   a=list_of_lists_to_modify
-    del a[0][-1]
-    del a[1][0]
-    del a[1][-1]
-    del a[2][0]
-    del a[2][1]
-    return a
+   list1= list_of_lists_to_modify[0][0:2]
+   list2= list_of_lists_to_modify[1][1:4]
+   list3= list_of_lists_to_modify[2][-2::]
+   final_list= [list1, list2, list3]
+    return final_test
