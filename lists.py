01c717775b27e88e2d5685b2fd4bd3759a3415ff
ThiagooAguilar <162721875+ThiagooAguilar@users.noreply.github.com>
2024-05-23 20:13:12 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,13 +1,12 @@
 # Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    if len(list_to_remove_elements)>6:
-       del  list_to_remove_elements[0,3,5]
-    elif len(list_to_remove_elements)<6:
-        del list_to_remove_elements[0,3]
-    elif len(list_to_remove_elements)<5:
-        del list_to_remove_elements[0]
-    return list_to_remove_elements
+ if len(lst) >= 1:
+        lst.remove(lst[0])  
+    if len(lst) >= 5:
+        lst.remove(lst[4]) 
+    if len(lst) >= 5:
+        lst.remove(lst[4])
 
 
 
@@ -27,14 +26,17 @@ def is_empty(list_to_check):
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1)<3 or len(list_to_compare2)<3:
         return False
-    elif list_to_compare1[2] == list_to_compare2[2]:
+    elif list_to_compare1[1] == list_to_compare2[1]:
         return True
     else:
-        return Flase 
-
-def list_of_lists(list_of_lists_to_modify):
-   list1= list_of_lists_to_modify[0][0:2]
-   list2= list_of_lists_to_modify[1][1:4]
-   list3= list_of_lists_to_modify[2][-2::]
-   final_list= [list1, list2, list3]
-    return final_test
+        return False 
+        
+def list_of_lists(lists):
+    if len(lists[0]) > 2:
+        lists[0] = lists[0][:2]
+    if len(lists[1]) > 1:
+        lists[1] = lists[1][1:4]
+        
+    if len(lists[2]) > 2:
+        lists[2] = lists[2][-2:]
+    return lists
